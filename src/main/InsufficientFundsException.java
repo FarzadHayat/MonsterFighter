00af40e026d038948e146876cfcb9bf223ee33fd
Farzad Hayatbakhsh <fha62@uclive.ac.nz>
2022-04-15 09:19:40 +1200

    Added UnsufficientFundsException class

---
+++ src/main/InsufficientFundsException.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Class InsufficientFundsException
+ * Extends Exception:
+Exception class for catching an insufficient funds error.
+ */
+@SuppressWarnings("serial")
+public class InsufficientFundsException extends Exception {
+	
+	public InsufficientFundsException () {}
+	
+    public InsufficientFundsException (String message) {
+    	super(message);
+    };
+}
