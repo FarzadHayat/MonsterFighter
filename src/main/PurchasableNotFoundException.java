eebccc2a7805dbe9e3b083ccd12a2426766b7640
Farzad Hayatbakhsh <fha62@uclive.ac.nz>
2022-04-15 10:42:42 +1200

    Added PurchasableNotFoundException.

---
+++ src/main/PurchasableNotFoundException.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Class PurchasableNotFoundException
+ * Extends Exception:
+Exception class for catching a Purchasable not found in the inventory error.
+ */
+@SuppressWarnings("serial")
+public class PurchasableNotFoundException extends Exception {
+	
+	public PurchasableNotFoundException () {}
+	
+    public PurchasableNotFoundException (String message) {
+    	super(message);
+    };
+}
