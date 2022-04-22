7db6cf53036a7164cf6c0f233e41361a702b7cba
Daniel <zrc13@uclive.ac.nz>
2022-04-22 15:07:44 +1200

    Added method in Score to calculate the player's final score and added new ScoreTest to test Score

--- src/main/Score.java
+++ src/main/Score.java
@@ -1,70 +1,121 @@
 package main;
 
 public class Score {
-    
+   
     private int dayBattlesWon;
     private int dayBattlesLost;
     private int score;
     private int totalBattlesWon;
     private int totalBattlesLost;
     private GameEnvironment game;
-    
+   
+    private int battlesWeight = 100;
+    private int balanceWeight = 10;
+   
     public Score(GameEnvironment game) {
-	this.game = game;
+    	this.game = game;
     }
-    
+   
+    /**
+     * @return number of battles won in a day
+     */
     public int getDayBattlesWon() {
-	return dayBattlesWon;
+    	return dayBattlesWon;
     }
-    
+   
+    /**
+     * @param battlesWon number of battles won in a day
+     */
     public void setDayBattlesWon(int battlesWon) {
-	this.dayBattlesWon = battlesWon;
+    	this.dayBattlesWon = battlesWon;
     }
-    
+   
+    /**
+     * @return number of battles lost in a day
+     */
     public int getDayBattlesLost() {
-	return dayBattlesLost;
+    	return dayBattlesLost;
     }
-    
+   
+    /**
+     * @param battlesLost number of battles lost in a day
+     */
     public void setDayBattlesLost(int battlesLost) {
-	this.dayBattlesLost = battlesLost;
+    	this.dayBattlesLost = battlesLost;
     }
-    
+   
+    /**
+     * @return player's score
+     */
     public int getScore() {
-	return score;
+    	return score;
     }
-    
+   
+    /**
+     * @param score the player's score
+     */
     public void setScore(int score) {
-	this.score = score;
+    	this.score = score;
     }
-    
+   
+    /**
+     * @return total battles won in the game
+     */
     public int getTotalBattlesWon() {
-	return totalBattlesWon;
+    	return totalBattlesWon;
     }
-    
+   
+    /**
+     * @param battlesWon number of battles won
+     */
     public void setTotalBattlesWon(int battlesWon) {
-	this.totalBattlesWon = battlesWon;
+    	this.totalBattlesWon = battlesWon;
     }
-    
+   
+    /**
+     * @return total battles lost in the game
+     */
     public int getTotalBattlesLost() {
-	return totalBattlesLost;
+    	return totalBattlesLost;
     }
-    
+   
+    /**
+     * @param battlesLost number of battles lost
+     */
     public void setTotalBattlesLost(int battlesLost) {
-	this.totalBattlesLost = battlesLost;
+    	this.totalBattlesLost = battlesLost;
     }
-    
+   
+    /**
+     * Add one to the battles won in a day and total battles won
+     */
     public void addBattlesWon() {
-	this.dayBattlesWon++;
-	this.totalBattlesWon++;
+		this.dayBattlesWon++;
+		this.totalBattlesWon++;
     }
-    
+   
+    /**
+     * Add one to the battles lost in a day and total battles lost
+     */
     public void addBattlesLost() {
-	this.dayBattlesLost++;
-	this.totalBattlesLost++;
+		this.dayBattlesLost++;
+		this.totalBattlesLost++;
     }
-    
+   
+    /**
+     * Reset battles won/lost in a day to zero
+     */
     public void resetDayBattles() {
-	this.dayBattlesLost = 0;
-	this.dayBattlesWon = 0;
+		this.dayBattlesLost = 0;
+		this.dayBattlesWon = 0;
+    }
+   
+    /**
+     * Calculate the player's final score based on the number of battles won and balance remaining
+     * @return player's final calculated score
+     */
+    public int getFinalScore() {
+		score += totalBattlesWon*battlesWeight + game.getBalance()*balanceWeight;
+		return score;
     }
 }
---
+++ src/test/ScoreTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.GameEnvironment;
+import main.InvalidValueException;
+import main.Score;
+
+class ScoreTest {
+
+    private GameEnvironment game;
+    private Score scoreSystem;
+   
+    @BeforeEach
+    void setUp() throws Exception {
+    	game = new GameEnvironment();
+    	scoreSystem = new Score(game);
+    }
+
+    @Test
+    public void testAddBattlesWon() {
+		scoreSystem.addBattlesWon();
+		assertEquals(1, scoreSystem.getDayBattlesWon());
+		assertEquals(1, scoreSystem.getTotalBattlesWon());
+    }
+   
+    @Test
+    public void testAddBattlesLost() {
+		scoreSystem.addBattlesLost();
+		assertEquals(1, scoreSystem.getDayBattlesLost());
+		assertEquals(1, scoreSystem.getTotalBattlesLost());
+    }
+   
+    @Test
+    public void testResetDayBattles() {
+		scoreSystem.addBattlesWon();
+		scoreSystem.addBattlesLost();
+		scoreSystem.resetDayBattles();
+		assertEquals(1, scoreSystem.getTotalBattlesLost());
+		assertEquals(1, scoreSystem.getTotalBattlesWon());
+		assertEquals(0, scoreSystem.getDayBattlesLost());
+		assertEquals(0, scoreSystem.getDayBattlesWon());
+    }
+   
+    @Test
+    public void testGetFinalScore() throws InvalidValueException {
+		game.setBalance(100);
+		scoreSystem.setTotalBattlesWon(10);
+		assertEquals(2000, scoreSystem.getFinalScore());
+    }
+
+}
