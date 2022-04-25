0abcbe48717be98146f2bb7f3b52a5718af9fa5c
Farzad Hayatbakhsh <fha62@uclive.ac.nz>
2022-04-25 12:36:44 +1200

    Added empty CommandLineTest class.

---
+++ src/test/CommandLineTest.java
@@ -0,0 +1,134 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CommandLineTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCommandLine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetupCmdGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectPlayerName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectNumDays() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectDifficulty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectStartingMonster() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectMonsterName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewShop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewMonster() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUseItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewBattles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewBattle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlayBattle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewTeam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewInventory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewStats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPrintStats() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGoBack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewHome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testViewSleep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testProperCase() {
+		fail("Not yet implemented");
+	}
+
+}
