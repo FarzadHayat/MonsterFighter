0e2b3685e36d97a13440016ce0d0d8849b09e9d5
Daniel <zrc13@uclive.ac.nz>
2022-04-30 02:59:04 +1200

    Completed Start screen which allows player to play or quit

---
+++ src/gui/StartScreen.java
@@ -0,0 +1,79 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+import main.*;
+import java.awt.EventQueue;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class StartScreen {
+
+	private JFrame window;
+	private GraphicalUserInterface gui;
+	
+	
+	public void closeWindow() {
+		window.dispose();
+	}
+	
+	public void finishedWindow() {
+		gui.closeStartScreen(this);
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StartScreen(GraphicalUserInterface gui) {
+		this.gui = gui;
+		initialize();
+		window.setVisible(true);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		window = new JFrame();
+		window.setTitle("MonsterFighter - Start");
+		window.setResizable(false);
+		window.setBounds(100, 100, 800, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setLayout(null);
+		
+		JLabel titleLabel = new JLabel("Monster Fighter");
+		titleLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setBounds(250, 35, 300, 50);
+		window.getContentPane().add(titleLabel);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gui.setGame(new GameEnvironment());
+				gui.launchSetupScreen();
+				finishedWindow();
+			}
+		});
+		btnStart.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnStart.setBounds(250, 231, 300, 71);
+		window.getContentPane().add(btnStart);
+		
+		JButton btnQuit = new JButton("Quit");
+		btnQuit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				finishedWindow();
+			}
+		});
+		btnQuit.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnQuit.setBounds(250, 371, 300, 71);
+		window.getContentPane().add(btnQuit);
+
+		
+	}
+}
