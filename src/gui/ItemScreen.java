29fccbafc000ce0690fd4eef23fa716e67e537a6
Daniel <zrc13@uclive.ac.nz>
2022-05-03 18:27:45 +1200

    Completed ItemScreen except function to use item on selectedMonster

--- src/gui/ItemScreen.java
+++ src/gui/ItemScreen.java
@@ -2,8 +2,19 @@ package gui;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
+
+import exceptions.InvalidValueException;
+import exceptions.InventoryFullException;
+import exceptions.NotFoundException;
+import items.IncreaseCritRate;
+import items.IncreaseDamage;
+import items.LevelUp;
+
 import javax.swing.JButton;
+
+import java.awt.Color;
 import java.awt.Font;
 
 import main.*;
@@ -19,6 +30,10 @@ public class ItemScreen {
 	private GameEnvironment game;
 	private Player player;
 	private Score score;
+	
+	private MonsterInventory monsters;
+	private JButton selectedButton;
+	private Monster selectedMonster;
 	
 	private Item item;
 	
@@ -39,6 +54,7 @@ public class ItemScreen {
 		game = gui.getGame();
 		player = game.getPlayer();
 		score = game.getScoreSystem();
+		monsters = player.getMonsters();
 		initialize();
 		window.setVisible(true);
 	}
@@ -48,7 +64,7 @@ public class ItemScreen {
 	 */
 	private void initialize() {
 		window = new JFrame();
-		window.setTitle("MonsterFighter - Items");
+		window.setTitle("MonsterFighter - Item");
 		window.setResizable(false);
 		window.setBounds(100, 100, 800, 600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -70,5 +86,91 @@ public class ItemScreen {
 		backButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		backButton.setBounds(650, 30, 100, 30);
 		window.getContentPane().add(backButton);
+		
+		JButton btnUse = new JButton("Use");
+		btnUse.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnUse.setBounds(631, 508, 119, 44);
+		window.getContentPane().add(btnUse);
+		
+		JButton btnSell = new JButton("Sell");
+		btnSell.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int result = JOptionPane.showConfirmDialog(null, String.format("Are you sure you want to sell\n%s item for %s gold?", item.getName(), (int)(item.getCost()*item.getRefundAmount())));
+				if(result == 0) {
+					try {
+						AlertBox.infoBox(item.sell(), "Item Sold!");
+						gui.launchHomeScreen();
+						finishedWindow();
+					} catch (NotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (InvalidValueException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		btnSell.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnSell.setBounds(631, 453, 119, 44);
+		window.getContentPane().add(btnSell);
+		
+		JPanel monstersPanel = new JPanel();
+		monstersPanel.setLayout(null);
+		monstersPanel.setBounds(10, 87, 766, 465);
+		window.getContentPane().add(monstersPanel);
+		
+		JLabel txtDescription = new JLabel();
+		txtDescription.setVerticalAlignment(SwingConstants.TOP);
+		txtDescription.setHorizontalAlignment(SwingConstants.CENTER);
+		txtDescription.setBounds(226, 11, 325, 85);
+		monstersPanel.add(txtDescription);
+		txtDescription.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtDescription.setText(item.getDescription());
+		txtDescription.setBackground(null);
+		
+		int yPos = 100;
+		int xPos = 215;
+		int numButton = 1;
+		for (Monster monster : monsters.getList()) {
+			JButton monsterButton = new JButton(monster.getName());
+			if (monster.getName().length() > 10) {				
+				monsterButton.setText("<html><center>" + monster.getName().replaceFirst(" ", "<br>") + "</centre></html>");
+			}
+			monsterButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					selectedMonster = monster;
+					if(selectedButton != null) {
+						selectedButton.setBackground(null);
+					}
+					selectedButton = monsterButton;
+					selectedButton.setBackground(Color.lightGray);
+				}
+			});
+			monsterButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			monsterButton.setBounds(xPos, yPos, 160, 90);
+			monstersPanel.add(monsterButton);
+			xPos += 190;
+			numButton++;
+			if(numButton == 3) {
+				yPos += 120;
+				xPos = 215;
+			}
+		}
+	}
+	
+	public static void main(String[]args){
+		GraphicalUserInterface gui = new GraphicalUserInterface();
+		gui.setGame(new GameEnvironment());
+		gui.getGame().setupGame();
+		for(int i = 0; i < 4; i++) {
+			try {
+				gui.getGame().getPlayer().getMonsters().add(gui.getGame().getAllMonsters().getList().get(i).clone());
+			} catch (InventoryFullException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		new ItemScreen(gui, new IncreaseDamage(gui.getGame()));
 	}
 }
