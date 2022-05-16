5c342a53af314f51ca983984014ff6f6cd8383d5
Daniel <zrc13@uclive.ac.nz>
2022-05-17 01:34:22 +1200

    Completed unit test for Score

--- src/test/ScoreTest.java
+++ src/test/ScoreTest.java
@@ -21,21 +21,42 @@ class ScoreTest {
     }
 
     @Test
-    public void testAddBattlesWon() {
+    public void testAddBattlesWon1() {
+    	//Won 1 battle
 		scoreSystem.addBattlesWon();
 		assertEquals(1, scoreSystem.getDayBattlesWon());
 		assertEquals(1, scoreSystem.getTotalBattlesWon());
     }
+    
+    @Test
+    public void testAddBattlesWon2() {
+    	//Won 2 battles
+		scoreSystem.addBattlesWon();
+		scoreSystem.addBattlesWon();
+		assertEquals(2, scoreSystem.getDayBattlesWon());
+		assertEquals(2, scoreSystem.getTotalBattlesWon());
+    }
    
     @Test
-    public void testAddBattlesLost() {
+    public void testAddBattlesLost1() {
+    	//Lost 1 battle
 		scoreSystem.addBattlesLost();
 		assertEquals(1, scoreSystem.getDayBattlesLost());
 		assertEquals(1, scoreSystem.getTotalBattlesLost());
     }
+    
+    @Test
+    public void testAddBattlesLost2() {
+    	//Lost 2 battles
+		scoreSystem.addBattlesLost();
+		scoreSystem.addBattlesLost();
+		assertEquals(2, scoreSystem.getDayBattlesLost());
+		assertEquals(2, scoreSystem.getTotalBattlesLost());
+    }
    
     @Test
     public void testResetDayBattles() {
+    	//Reset daily battles but keep total battles 
 		scoreSystem.addBattlesWon();
 		scoreSystem.addBattlesLost();
 		scoreSystem.resetDayBattles();
@@ -44,11 +65,38 @@ class ScoreTest {
 		assertEquals(0, scoreSystem.getDayBattlesLost());
 		assertEquals(0, scoreSystem.getDayBattlesWon());
     }
+    
+    @Test
+    public void testScoreBonus() {
+    	//Calculate bonus from 10 battles won and balance of 100
+    	scoreSystem.setTotalBattlesWon(10);
+    	assertEquals(2000, scoreSystem.scoreBonus());
+    }
    
     @Test
     public void testGetFinalScore() throws InvalidValueException {
+    	//Calculate final score plus bonuses
+    	//10 battles won and player balance of 100
 		scoreSystem.setTotalBattlesWon(10);
 		assertEquals(3000, scoreSystem.finalScore());
+    }
+    
+    @Test
+    public void testAddScore1() throws InvalidValueException {
+    	scoreSystem.addScore(1000);
+    	assertEquals(1000, scoreSystem.getDayScore());
+    	assertEquals(2000, scoreSystem.getTotalScore());
+    }
+    
+    @Test
+    public void testAddScore2() {
+    	try {
+			scoreSystem.addScore(-1);
+		} catch (InvalidValueException e) {
+			assertEquals("Cannot be a negative value!", e.getMessage());
+		}
+    	assertEquals(0, scoreSystem.getDayScore());
+    	assertEquals(1000, scoreSystem.getTotalScore());
     }
 
 }
