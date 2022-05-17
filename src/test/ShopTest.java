c293bed6314993bf0a8543ab8c1666e751075d76
Daniel <zrc13@uclive.ac.nz>
2022-05-17 23:22:18 +1200

    Completed unit test for Shop class

--- src/test/ShopTest.java
+++ src/test/ShopTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,22 +12,48 @@ import main.*;
 class ShopTest {
 
 	private GameEnvironment game;
+	private Shop shop;
+	private Player player;
+	private MonsterInventory monsters;
+	private ItemInventory items;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		game = new GameEnvironment();
+		game.setupGame();
+		shop = game.getShop();
+		player = game.getPlayer();
+		monsters = shop.getMonsters();
+		items = shop.getItems();
 	}
 
 	
 	@Test
 	void testRandomise() {
-		fail("Not yet implemented");
+		ArrayList<Monster> monstersList = monsters.getList();
+		ArrayList<Item> itemsList = items.getList();
+		shop.randomise();
+		assertTrue(monstersList != monsters.getList());
+		assertTrue(itemsList != items.getList());
 	}
 
 	
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+		String result = String.format("\nBalance: %s\n", player.getBalance());
+    	int start = 1;
+    	result += "\n===== MONSTERS =====\n\n";
+    	for (Monster monster : monsters.getList()) {
+    		result += String.format("%s: %s\n", start, monster);
+    		start++;
+    	}
+    	result += "\n===== ITEMS =====\n\n";
+    	for (Item item : items.getList()) {
+    		result += String.format("%s: %s\n", start, item);
+    		start++;
+    	}
+    	result += String.format("\n%s: Go back", start);
+    	assertEquals(result, shop.toString());
 	}
 
 }
